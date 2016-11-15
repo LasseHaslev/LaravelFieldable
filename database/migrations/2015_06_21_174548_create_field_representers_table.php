84d94932cd3041f8769f0b81708cc04c965ad27c
Lasse S. Haslev <lasse@haslev.no>
2016-11-16 00:38:43 +0100

    added migrations

---
+++ database/migrations/2015_06_21_174548_create_field_representers_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFieldRepresentersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('field_representers', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->string('identifier');
+
+            // $table->string('type');
+            // Should be
+            // $table->integer( 'type_id' )
+            $table->string('description')
+                ->default( '' );
+            $table->boolean('is_repeatable')
+                ->default( false );
+
+            $table->integer('order')
+                ->unsigned()
+                ->default(0);
+
+            // $table->boolean( 'is_group' );
+            // $table->integer('field_representer_id')
+                // ->unsigned()
+                // ->nullable()
+                // ->index();
+            // $table->foreign('field_representer_id')
+                // ->references('id')
+                // ->on('field_representers')
+                // ->onDelete('cascade');
+
+            $table->integer('fieldable_id')
+                ->unsigned()
+                ->nullable();
+            $table->string('fieldable_type')
+                ->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('field_representers');
+    }
+}
---
+++ database/migrations/2015_06_21_174600_create_field_values_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFieldValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('field_values', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('value');
+
+            $table->integer('field_representer_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('field_representer_id')
+                ->references('id')
+                ->on('field_representers')
+                ->onDelete('cascade');
+
+            $table->integer('fieldable_id')
+                ->unsigned()
+                ->nullable();
+            $table->string('fieldable_type')
+                ->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('field_values');
+    }
+}

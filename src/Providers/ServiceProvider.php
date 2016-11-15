f4adcd7e5835fdfabe7d67a3afc3c9495480e976
Lasse S. Haslev <lasse@haslev.no>
2016-11-15 23:42:18 +0100

    Added database setup for tests

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,24 @@
+<?php namespace LasseHaslev\LaravelCms\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}

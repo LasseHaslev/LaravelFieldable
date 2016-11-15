c464cfb51092670da227cfc8a244ba59e5b6b1a2
Lasse S. Haslev <lasse@haslev.no>
2016-11-15 23:08:40 +0100

    Setup test settings

---
+++ tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class FirstTest
+ * @author yourname
+ */
+class FirstTest extends TestCase
+{
+    /** @test */
+    public function test() {
+        $this->assertTrue( true );
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class BaseTestCase
+ * @author Lasse S. Haslev
+ */
+class TestCase extends Orchestra\Testbench\TestCase
+{
+}

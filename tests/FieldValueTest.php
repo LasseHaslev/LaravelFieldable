882166d327b7a483a636a7f393cdef1562682e4a
Lasse S. Haslev <lasse@haslev.no>
2016-11-23 15:31:13 +0100

    Removed todo in tests

--- tests/FieldValueTest.php
+++ tests/FieldValueTest.php
@@ -161,13 +161,7 @@ class FieldValueTest extends TestCase
     // etc. image.id to image object and image object to image.id
     // Check if we have an interface for that
 
-    // Can add muliple values if repeatable is true
-    // Can NOT add muliple values if repeatable is true
-    // if multiple values is added to fieldable if is_repeatable is false. Just update value
-
     // Can update order for values
-
-    // Write logic to handle the trait to add/handle FieldValue
 
     // LATER
     // FieldValue obay FieldRepresenter minimum and maximum values
